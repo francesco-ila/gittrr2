cd6bbe3f8cb01a9a2e3d86d9a0130bf3d584b412
francesco-ila <francesco.ila02@gmail.com>
2020-04-08 13:06:29 +0200

    1st commit noob 2 try

---
+++ cmake-build-debug/CMakeFiles/gittrr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gittrr.dir/main.cpp.obj"
+  "gittrr.exe"
+  "gittrr.exe.manifest"
+  "gittrr.pdb"
+  "libgittrr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gittrr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
